f444e01bc78690ca685d721464b64087e88f2251
LeDinhQuoc24 <kudo2412@gmail.com>
2020-03-31 13:11:13 +0700

    addfile

---
+++ src/X1_Tong_quan_ung_dung_web/Bai_Tap/Ung_dung_Product_Discount_CalculatorAssignment.java
@@ -0,0 +1,19 @@
+package X1_Tong_quan_ung_dung_web.Bai_Tap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Ung_dung_Product_Discount_CalculatorAssignment")
+public class Ung_dung_Product_Discount_CalculatorAssignment extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
